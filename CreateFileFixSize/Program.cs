ec1ea80316cb91bdf4a397545cc6cc4fdc20b248
LeeRising <leerising773@gmail.com>
2017-07-05 15:29:14 +0300

    Create repo and project

---
+++ CreateFileFixSize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace CreateFileFixSize
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("File size(Mib) ");
+            var fileSize = Convert.ToInt32(Console.ReadLine());
+            var fileBytes = new byte[fileSize * 1048576];
+            new Random().NextBytes(fileBytes);
+            using (var writer = new BinaryWriter(File.Create($"testfile{fileSize}Mb")))
+                writer.Write(fileBytes);
+        }
+    }
+}
